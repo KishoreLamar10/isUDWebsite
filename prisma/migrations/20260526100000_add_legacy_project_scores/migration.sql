a6627cf522cfff11c955b5ca95a77c6f0b3adae2
Kishorekumar Avudai Nayagam <balakishore619@gmail.com>
2026-05-28 15:39:23 -0400

    Track legacy score migration

---
+++ prisma/migrations/20260526100000_add_legacy_project_scores/migration.sql
@@ -0,0 +1,7 @@
+ALTER TABLE "Project"
+ADD COLUMN "legacyApplicableCredits" DOUBLE PRECISION,
+ADD COLUMN "legacyEarnedCredits" DOUBLE PRECISION,
+ADD COLUMN "legacyBonusCredits" DOUBLE PRECISION,
+ADD COLUMN "legacyAwardPercentage" DOUBLE PRECISION,
+ADD COLUMN "legacyChapterApplicable" TEXT,
+ADD COLUMN "legacyChapterEarned" TEXT;
